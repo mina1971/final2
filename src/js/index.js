2e9e6af74d8d6720f727c9111c09c6f3f5151845
mina1971 <jommina62@gmail.com>
2021-04-14 17:13:23 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min';
+import '../sass/style.scss';
+import '@fortawesome/fontawesome-free/js/all.min';
+
+
+
