54b3b221d38a8cf3a4d5eba74e0d9909fe1c4362
mina1971 <jommina62@gmail.com>
2021-04-17 16:53:12 +0200

    adding map

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min';
+import '@fortawesome/fontawesome-free/js/all.min';
+import '../sass/style.scss';
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const { write } = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    publicPath:'/',
+    path: path.resolve(__dirname, "build"),
+    filename: "js/bundle.js",
+   },
+   module: {
+    rules: [
+
+      {
+        test: /\.(sc|sa|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options:{
+              publicPath:'../'
+            }
+          },
+          "css-loader",
+          "sass-loader",
+          ],
+          },
+          {
+            test: /\.(gif|png|jpe?g)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'images',
+                }
+              }
+            ]
+          },
+          {
+            test: /\.(svg|eot|woff2|ttf)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts',
+                }
+              }
+            ]
+          },
+          
+          {
+            test:/\.html$/i,
+            loader:'html-loader',
+          },
+          {
+            test: require.resolve("jquery"),
+            loader: "expose-loader",
+            options: {
+              exposes: ["$", "jQuery"],
+            },
+          },
+        ],
+      },
+
+    devServer: {
+     contentBase: path.join(__dirname, 'build'),
+    //compress: true,
+    port: 9000,
+    writeToDisk:true,
+    stats:'errors-only',
+    open: true
+    },
+
+   plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: "./src/book1.html",
+      filename: "book1.html",
+    }),
+    
+    new HtmlWebpackPlugin({
+      template: "./src/book2.html",
+      filename: "book2.html",
+    }),
+    
+    new MiniCssExtractPlugin(),
+   ],
+   };
