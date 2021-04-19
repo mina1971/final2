37c82f50e77a63ec83eb4575d7b6f380bcb630d4
mina1971 <jommina62@gmail.com>
2021-04-19 21:49:43 +0200

    second disine

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,15 @@ import 'popper.js/dist/popper.min';
 import '@fortawesome/fontawesome-free/js/all.min';
 import '../sass/style.scss';
 
-function render_arrows() {
-    var vidtag;
-    vidtag = document.getElementById('arrows');
-    vidtag.src = mov;
-  }
+//function render_arrows() {
+ //   var vidtag;
+ //   vidtag = document.getElementById('arrows');
+ //   vidtag.src = mov;
+ // };
   
-  render_arrows();
+ // render_arrows();
 
+ 
+ 
 
+  
